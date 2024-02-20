eca3b7407398cf7e1cffe3a88805e46c83ef33b0
ommahmood <149751362+ommahmood@users.noreply.github.com>
2024-02-20 14:21:56 -0800

    Update TR_OLedCANReaderMain.ino

--- TR_OLedCANReaderMain.ino
+++ TR_OLedCANReaderMain.ino
@@ -2,57 +2,71 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <mcp_can.h>
 
-Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+#define OLED_RESET 4
+
+Adafruit_SSD1306 display(OLED_RESET); 
+//Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+char msgString[128];                        // Array to store serial string
+
+#define CAN0_INT 2                              // Set INT to pin 2
+MCP_CAN CAN0(10);                               // Set CS to pin 10
 
 int counter;
 
 void setup() {
-  Serial.begin(1000);
+  Serial.begin(115200);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
 
   display.display();
   delay(1000);
-
+  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
 
-  attachInterrupt(0, MCP2525_ISR, FAILING);
-  void MCP2515_ISR() {
-  Flag_Recv = 1;
-  }
-  
-
-  
+  // Initialize MCP2515 running at 8MHz with a baudrate of 1000kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_1000KBPS, MCP_8MHZ) == CAN_OK)
+    display.println("MCP2515 Initialized!");
+  else
+    display.println("Error Initializing MCP2515...");
+  display.display();
+  delay(3000);
+  display.clearDisplay();
+  display.setCursor(0,0);
+  CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
+  pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
+  display.print("MCP2515 Library Receive Example...");
+  display.display();
+  delay(2000);
+  display.clearDisplay();
 }
 
 void loop()
 {
-    if(Flag_Recv)                   // check if data was recieved
-    {
-      Flag_Recv = 0;                // clear flag
-      CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
-      canId = CAN.getCanId();
-      //Print data to the serial console 
-      //and the LCD display
-      Serial.println("CAN_BUS GET DATA!");
-      Serial.print("CAN ID: ");
-      Serial.println(canId);
-      lcd.setCursor(0, 0);
-      lcd.print("CAN ID: ");
-      lcd.print(canId);
-      lcd.setCursor(0, 2);
-      Serial.print("data len = ");Serial.println(len);
-      //This loops through each byte of data and prints it
-      for(int i = 0; i<len; i++)    // print the data
-      {
-        Serial.print(buf[i]);Serial.print("\t");
-        lcd.print(buf[i]);
-        lcd.print(" ");
-      }
-      Serial.println();
-      delay(50);
-    }
+    //Initialize OLED Printing
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+    
+    if(!digitalRead(CAN0_INT)){
+      CAN0.readMsgBuf(&rxId, &len, rxBuf); // Read data: len = data length, buf = data byte(s)
+      int motorID = rxId - 0x200;
+      display.print("Motor #");
+      display.println(motorID,HEX);
+      display.println("Data:");
+      /*DOES NOT WORK
+      for(counter = 0; counter < 128; counter++){
+        display.print(msgString[counter]);
+      } HAVE TO PARSE ARRAY AND CREATE VALUES*/
+    }  
+      display.display();
+      delay(1000);
+      display.clearDisplay();
 }
