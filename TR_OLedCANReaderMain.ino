1edfaed7059a169bbef7c742a09fb618ec8e7197
ommahmood <149751362+ommahmood@users.noreply.github.com>
2024-02-26 17:09:38 -0800

    Update TR_OLedCANReaderMain.ino

--- TR_OLedCANReaderMain.ino
+++ TR_OLedCANReaderMain.ino
@@ -6,7 +6,7 @@
 
 #define OLED_RESET 4
 
-Adafruit_SSD1306 display(OLED_RESET); 
+Adafruit_SSD1306 display(OLED_RESET);
 //Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 
 long unsigned int rxId;
@@ -20,7 +20,7 @@ int velocityHigh, velocityLow;
 int torqueHigh, torqueLow;
 int temperatureHigh, temperatureLow;
 // Variables to store complete data
-int angle;
+int angles;
 int velocity;
 int torque;
 int temperature;
@@ -42,14 +42,14 @@ void setup() {
   display.setCursor(0, 0);
 
   // Initialize MCP2515 running at 8MHz with a baudrate of 1000kb/s and the masks and filters disabled.
-  if(CAN0.begin(MCP_ANY, CAN_1000KBPS, MCP_8MHZ) == CAN_OK)
+  if (CAN0.begin(MCP_ANY, CAN_1000KBPS, MCP_8MHZ) == CAN_OK)
     display.println("MCP2515 Initialized!");
   else
     display.println("Error Initializing MCP2515...");
   display.display();
   delay(3000);
   display.clearDisplay();
-  display.setCursor(0,0);
+  display.setCursor(0, 0);
   CAN0.setMode(MCP_NORMAL);                     // Set operation mode to normal so the MCP2515 sends acks to received data.
   pinMode(CAN0_INT, INPUT);                            // Configuring pin for /INT input
   display.print("MCP2515 Library Receive Example...");
@@ -60,48 +60,48 @@ void setup() {
 
 void loop()
 {
-    //Initialize OLED Printing
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0,0);
-    
-    if(!digitalRead(CAN0_INT)){
-      CAN0.readMsgBuf(&rxId, &len, rxBuf); // Read data: len = data length, buf = data byte(s)
-      int motorID = rxId - 0x200;
-      display.print("Motor #");
-      display.println(motorID,HEX);
-      display.println("Data:");
-      /*DOES NOT WORK
+  //Initialize OLED Printing
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+
+  if (!digitalRead(CAN0_INT)) {
+    CAN0.readMsgBuf(&rxId, &len, rxBuf); // Read data: len = data length, buf = data byte(s)
+    int motorID = rxId - 0x200;
+    display.print("Motor #");
+    display.println(motorID, HEX);
+    display.println("Data:");
+    /*DOES NOT WORK
       for(counter = 0; counter < 128; counter++){
-        display.print(msgString[counter]);
+      display.print(msgString[counter]);
       } HAVE TO PARSE ARRAY AND CREATE VALUES*/
-      // Parse the msgString array
-      angleHigh = msgString[0];
-      angleLow = msgString[1];
-      velocityHigh = msgString[2];
-      velocityLow = msgString[3];
-      torqueHigh = msgString[4];
-      torqueLow = msgString[5];
-      temperatureHigh = msgString[6];
-      temperatureLow = msgString[7];
+    // Parse the msgString array
+    angleHigh = msgString[0];
+    angleLow = msgString[1];
+    velocityHigh = msgString[2];
+    velocityLow = msgString[3];
+    torqueHigh = msgString[4];
+    torqueLow = msgString[5];
+    temperatureHigh = msgString[6];
+    temperatureLow = msgString[7];
 
-      angle = (angleHigh << 8) | angleLow;
-      velocity = (velocityHigh << 8) | velocityLow;
-      torque = (torqueHigh << 8) | torqueLow;
-      temperature = (temperatureHigh << 8) | temperatureLow;
-      display.print("Angle: ");
-      display.println(angle);
+    angles = (angleHigh << 8) | angleLow;
+    velocity = (velocityHigh << 8) | velocityLow;
+    torque = (torqueHigh << 8) | torqueLow;
+    temperature = (temperatureHigh << 8) | temperatureLow;
+    display.print("Angle: ");
+    display.println(angles);
 
-      display.print("Velocity: ");
-      display.println(velocity);
-      
-      display.print("Torque: ");
-      display.println(torque);
-      
-      display.print("Temperature: ");
-      display.println(temperature);
-    }  
-      display.display();
-      delay(1000);
-      display.clearDisplay();
+    display.print("Velocity: ");
+    display.println(velocity);
+
+    display.print("Torque: ");
+    display.println(torque);
+
+    display.print("Temperature: ");
+    display.println(temperature);
+  }
+  display.display();
+  delay(1000);
+  display.clearDisplay();
 }
